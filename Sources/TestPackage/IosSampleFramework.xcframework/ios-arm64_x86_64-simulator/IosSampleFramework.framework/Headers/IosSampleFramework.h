a702ddfab2baa6524f9dd54fb30073b8c61ffc14
Ojas Sharma <ojas.sharma@bugsmirror.com>
2024-08-08 16:16:44 +0530

    added xcframework into is and done changes accordingly

---
+++ Sources/TestPackage/IosSampleFramework.xcframework/ios-arm64_x86_64-simulator/IosSampleFramework.framework/Headers/IosSampleFramework.h
@@ -0,0 +1,18 @@
+//
+//  IosSampleFramework.h
+//  IosSampleFramework
+//
+//  Created by Ojas Sharma on 26/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IosSampleFramework.
+FOUNDATION_EXPORT double IosSampleFrameworkVersionNumber;
+
+//! Project version string for IosSampleFramework.
+FOUNDATION_EXPORT const unsigned char IosSampleFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IosSampleFramework/PublicHeader.h>
+
+
